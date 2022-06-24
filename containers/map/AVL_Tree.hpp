12a4913990a40aada3b37584fb5a189d98484a88
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-06-24 23:43:12 +0100

    start creating avl tree

--- containers/map/AVL_Tree.hpp
+++ containers/map/AVL_Tree.hpp
@@ -6,7 +6,7 @@
 /*   By: abbelhac <abbelhac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:17:09 by abbelhac          #+#    #+#             */
-/*   Updated: 2022/06/20 20:22:19 by abbelhac         ###   ########.fr       */
+/*   Updated: 2022/06/24 20:39:54 by abbelhac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ template < typename T >
         T       element;
         Node    *left;
         Node    *right;
+        Node    *parent;
         int     bf;
         int     height;
-    
-        Node(const T value) : element(value), left(nullptr), right(nullptr), bf(0), height(0) {}
-    
+        Node(const T &value) : element(value), left(nullptr), right(nullptr), bf(0), height(0) {}
+        Node(const T &value, int keep) : elment(value) { keep = 0; }
     };
 
-template < typename T, class Alloc >
+template < class T, class Alloc >
     class AvlTree
     {
         typedef T               value_type;
@@ -38,15 +38,29 @@ template < typename T, class Alloc >
         
         private :
                     node_pointer            *root;
-                    allocator_type  _alloc;
+                    allocator_type          _alloc;
                     
         public  :
 
                     AvlTree() : root(nullptr) {}
-                    AvlTree(value_type value) : root(_alloc.allocate(1, value)) {}
-                    ~AvlTree(){_alloc.deallocate(root, 1);}
-                    
-        
+                    AvlTree(value_type &value) : root(_alloc.allocate(1, value)) {}
+                    // ~AvlTree()
+                    // {
+                    //     _alloc.deallocate(root, 1);
+                    //     root = nullptr;
+                    // }
+
+                    node_pointer    clear_all(node_pointer node)
+                    {
+                        if (node != nullptr)
+                        {
+                            clear_all(node->left);
+                            clear_all(node->right);
+                            _alloc.deallocate(node, 1);
+                        }
+                        return nullptr;
+                    }
+            
     };
 
 #endif
