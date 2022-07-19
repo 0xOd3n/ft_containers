13dc86f001fb4288f15a65cdc4ab52713ca9eb5e
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-07-19 20:34:25 +0100

    Add : add memeber function : modifiers : swap / observers : key_comp, value_comp / operations : find, count, lower_bound, upper_bound, equal_range

--- containers/map/map.hpp
+++ containers/map/map.hpp
@@ -6,7 +6,7 @@
 /*   By: abbelhac <abbelhac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 19:20:22 by abbelhac          #+#    #+#             */
-/*   Updated: 2022/07/16 22:35:33 by abbelhac         ###   ########.fr       */
+/*   Updated: 2022/07/19 20:04:36 by abbelhac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -179,8 +179,6 @@ namespace ft
 				
 				void clear() {_tree.clear_all();}
 
-				// insert
-
 				pair<iterator, bool> insert (const value_type& val)
 				{
 					nodePtr	node = _tree.find(val);
@@ -215,8 +213,6 @@ namespace ft
 						first++;
 					}
 				}
-
-				// erase
 				
 				void	erase(iterator position)
 				{
@@ -246,9 +242,95 @@ namespace ft
 					}
 				}
 
-				
+				void	swap(map& other)
+				{
+					allocator_type alloc = _alloc;
+					key_compare	cmp = _cmp;
+					this->_alloc = other._alloc;
+					this->_cmp = other._cmp;
+					other._alloc = alloc;
+					other._cmp = cmp;
+					tree.swap(other._tree);
+				}
 
+				// Lookup
+
+				size_type	count(const key_type& k) const
+				{
+					nodePtr node = tree.find(ft::make(k, mapped_type()));
+					if (!node || node == _tree.getEnd())
+						return (0);
+					return (1);
+				}
+
+				iterator find(const key_type& k)
+				{
+					nodePtr	node = _tree.find(ft::make_pair(k, mapped_type()));
+					return (iterator(node));
+				}
+
+				const_iterator find(const key_type& k) const
+				{
+					nodePtr	node = _tree.find(ft::make_pair(k, mapped_type()));
+					return (const_iterator(node));
+				}
+
+				iterator	lower_bound(const key_type& k)
+				{
+					iterator iter = find(k);
+					if (iter.base() == _tree.getEnd())
+						iter = iterator(_tree.lowerBound(ft::make_pair(k, mapped_type())));
+					return (iter);
+				}
+
+				const iterator	lower_bound(const key_type& k) const
+				{
+					const iterator const_iter = find(k);
+					if (const_iter.base() == _tree.getEnd())
+						const_iter = const_iterator(_tree.lowerBound(ft::make_pair(k, mapped_type())));
+					return (const_iter);
+				}
 				
+				iterator upper_bound (const key_type& k)
+				{
+					iterator iter = find(k);
+					if (iter.base() != _tree.getEnd())
+						++iter;
+					else
+						iter = itererator(_tree.upperBound(ft::make_pair(k, mapped_type())));
+					return (iter);
+				}
+				const_iterator upper_bound (const key_type& k) const
+				{
+					const_iterator const_iter = find(k);
+					if (const_iter.base() != _tree.getEnd())
+						++const_iter;
+					else
+						const_iter = const_iterator(_tree.upperBound(ft::make_pair(k, mapped_type())));
+					return (const_iter);
+				}
+
+				pair<iterator, iterator> equal_range(const key_type& k)
+				{
+					return (ft::make_pair<iterator, iterator>(lower_bound(k), upper_bound(k)));
+				}
+				
+				pair<const_iterator, const_iterator> equal_range(const key_type& k)
+				{
+					return (ft::make_pair<const_iterator, const_iterator>(lower_bound(k), upper_bound(k)));
+				}
+				
+				// Observers
+
+				key_compare key_comp() const
+				{
+					return (this->_cmp);
+				}
+				
+				value_compare value_comp() const
+				{
+					return (value_compare(_cmp));
+				}
 				
 				// Allocator
 				
