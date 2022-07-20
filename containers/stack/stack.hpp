d171842e1108d68a529df7717c00625a15d3cc32
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-07-20 15:28:52 +0100

    Fix : visibility in member variables

--- containers/stack/stack.hpp
+++ containers/stack/stack.hpp
@@ -9,9 +9,10 @@ namespace ft
     template <class T, class Container = ft::vector<T> >
     class stack
     {
-        typedef T value_type;
-        typedef Container container_type;
-        typedef size_t size_type;
+        public:
+            typedef T value_type;
+            typedef Container container_type;
+            typedef size_t size_type;
 
         protected :
 
