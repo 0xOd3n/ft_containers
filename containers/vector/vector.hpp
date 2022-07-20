b96c93330bb9ed31e98a4af07a16a72763abb618
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-07-20 15:27:30 +0100

    Fix : constructors, type cast in loops

--- containers/vector/vector.hpp
+++ containers/vector/vector.hpp
@@ -37,19 +37,18 @@ namespace ft
                     allocator_type _alloc;
 
         public :
-                    explicit vector(const allocator_type& alloc = allocator_type()) : ptr(0), m_size(0), m_capacity(0) {}
+                    explicit vector(const allocator_type& alloc = allocator_type()) : ptr(0), m_size(0), m_capacity(0), _alloc(alloc){}
 
                     explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
-                    :   ptr(0), m_size(0), m_capacity(0), _alloc(alloc)
+                    : _alloc(alloc)
                     {
                         ptr = _alloc.allocate(n);
                         m_capacity = n;
-                        for (int i = 0; i < n; i++)
-                        {
+                        for (size_type i = 0; i < n; i++)
                             ptr[i] = val;
-                        }
                         m_size = n;
                     }
+
 
                     template <class InputIterator>
                     vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(), typename enable_if<!is_integral<InputIterator>::value, InputIterator>::type* = nullptr)
@@ -58,7 +57,7 @@ namespace ft
                         difference_type nt = last - first;
                         this->reserve(nt);
                         m_size = nt;
-                        for (int i = 0; i < nt; i++)
+                        for (difference_type i = 0; i < nt; i++)
                         {
                             ptr[i] = *(first + i);
                         }
@@ -84,7 +83,7 @@ namespace ft
                             this->m_size = copy.m_size;
                             this->_alloc = copy._alloc;
                             this->ptr = this->_alloc.allocate(copy.m_capacity);
-                            for (int i = 0; i < copy.m_size; i++)
+                            for (size_type i = 0; i < copy.m_size; i++)
                                 _alloc.construct(&ptr[i], copy.ptr[i]);
                         }
                         return (*this);
@@ -177,7 +176,7 @@ namespace ft
 
                             for (size_type i = 0; i < m_size; i++)
                             {
-                                _alloc.construct(&tmp[i], ptr[i]);
+                                tmp[i] = ptr[i];
                                 _alloc.destroy(&ptr[i]);
                             }
                             if (ptr)
@@ -196,14 +195,14 @@ namespace ft
                     reference	at(size_type n)
                     {
                         if (n >= m_size)
-                            throw std::out_of_range("out of range");
+                            throw std::out_of_range("vector");
                         return (ptr[n]);
                     }
 
 				    const_reference	at(size_type n) const
                     {
                         if (n >= m_size)
-                            throw std::out_of_range("out of range");
+                            throw std::out_of_range("vector");
                         return (ptr[n]);
                     }
 
@@ -228,6 +227,29 @@ namespace ft
                     }
                     
                     // modifiers
+
+                    template <class InputIterator>
+                    void assign (InputIterator first, InputIterator last,
+                                typename enable_if<!is_integral <InputIterator>::value, InputIterator>::type * = nullptr)
+                                //ANCHOR - SFINAE
+                    {
+                        size_type	i = -1;
+                        
+                        m_size = last - first;
+                        if (m_size <= m_capacity)
+                        {
+                            while (++i < m_size)
+                                ptr[i] = *(first++);
+                        }
+                        else
+                        {
+                            _alloc.deallocate(ptr, m_capacity);
+                            m_capacity = m_size;
+                            ptr = _alloc.allocate(m_capacity);
+                            while (++i < m_size)
+                                ptr[i] = *(first++);
+                        }
+                    }
 
                     void    assign(size_type n, const value_type& val)
                     {
@@ -257,27 +279,108 @@ namespace ft
                         m_size--;
                     }
 
-
-                    iterator	erase(iterator position)
+                    iterator insert (iterator position, const value_type& val)
                     {
-                        difference_type i = position - begin();
-
-                        _alloc.destroy(&ptr[i]);
-                        _shift_left(i, 1);
-                        m_size--;
-                        return (iterator(&ptr[i]));			
+                        size_type	new_cap;
+                        size_type	pos = position - this->begin();
+                        size_type	newSize = (pos >= m_size) ? pos : m_size;
+                        
+                        newSize++;
+                        if (newSize > m_capacity)
+                        {
+                            new_cap = ((m_capacity * 2) >= newSize) ? (m_capacity * 2) : newSize;
+                            this->reserve(new_cap);
+                        }
+                        m_size = newSize;
+                        while (--newSize > pos)
+                            ptr[newSize] = ptr[newSize - 1];
+                        ptr[pos] = val;
+                        return (this->begin() + pos);
                     }
 
-                    iterator	erase(iterator first, iterator last)
+                    void insert (iterator position, size_type n, const value_type& val)
                     {
-                        size_type	x = &*first - &*begin();
-                        size_type	y = &*last - &*begin();
+                        size_type	new_cap;
+                        size_type	shift = -1;
+                        size_type	pos = position - this->begin();
+                        size_type	newSize = ((pos + n) >= (m_size + n)) ? (pos + n) : (m_size + n);
+                        
+                        if (newSize > m_capacity)
+                        {
+                            new_cap = ((m_capacity * 2) >= newSize) ? (m_capacity * 2) : newSize;
+                            // new_cap = ((m_capacity * 2) >= (m_size + n)) ? (m_capacity * 2) : (m_size + n);
+                            this->reserve(new_cap);
+                        }
+                        shift = m_size - pos;
+                        m_size = newSize;
+                        while ((int)shift-- > 0 && newSize--)
+                            ptr[newSize] = ptr[newSize - n];
+                        while (n--)
+                            ptr[pos++] = val;
+                    }
 
-                        for (size_type z = x; z < y; z++)
-                            _alloc.destroy(&ptr[z]);
-                        _shift_left(x, y - x);
-                        m_size -= y - x;
-                        return (iterator(&ptr[x]));
+                    template <class InputIterator>
+                    void insert (iterator position, InputIterator first, InputIterator last,
+                                typename enable_if<!is_integral <InputIterator>::value, InputIterator>::type * = nullptr)
+                                //ANCHOR - SFINAE)
+                    {
+                        pointer		tmp;
+                        size_type	new_cap;
+                        size_type	shift = -1;
+                        size_type	n = last - first;
+                        size_type	pos = position - this->begin();
+                        size_type	newSize = ((pos + n - 1) >= (m_size + n - 1)) ? (pos + n - 1) : (m_size + n - 1);
+                        
+                        newSize++;
+                        if (newSize > m_capacity)
+                        {
+                            tmp = ptr;
+                            new_cap = ((m_capacity * 2) >= newSize) ? (m_capacity * 2) : newSize;
+                            // m_capacity = ((m_capacity * 2) >= (m_size + n)) ? (m_capacity * 2) : (m_size + n);
+                            // m_capacity = m_capacity * 2;
+                            this->reserve(new_cap);
+                        }
+                        shift = m_size - pos;
+                        m_size = newSize;
+                        while ((int)shift-- > 0 && newSize--)
+                            ptr[newSize] = ptr[newSize - n];
+                        while (n--)
+                            ptr[pos++] = *(first++);
+                    }
+
+                    iterator erase (iterator position)
+                    {
+                        size_type	pos = position - this->begin();
+                        
+                        while (pos < m_size - 1)
+                        {
+                            ptr[pos] = ptr[pos + 1];
+                            pos++;
+                        }
+                        pos = position - this->begin();
+                        if (pos < m_size)
+                            m_size--;
+                        return (this->begin() + pos);
+                    }
+
+                    iterator erase (iterator first, iterator last)
+                    {
+                        size_type	fpos = first - this->begin();
+                        size_type	lpos = last - this->begin();
+                        size_type	pos = last - first;
+                        size_type	size = m_size;
+                        
+                        if (lpos >= size)
+                            m_size = fpos;
+                        else
+                            m_size -= pos;
+                        while (lpos < size)
+                        {
+                            ptr[fpos] = ptr[lpos];
+                            fpos++;
+                            lpos++;
+                        }
+                        return (this->begin() + lpos);
                     }
 
                     void	swap(vector &x)
@@ -299,30 +402,6 @@ namespace ft
                     {
                         return (_alloc);
                     }
-
-                private:
-
-				void	_shift_right(size_type position, size_type n)
-				{
-					if (empty())
-						return;
-					for (size_type i = m_size - 1; i >= position; i--)
-					{
-						_alloc.construct(&ptr[i + n], ptr[i]);
-						_alloc.destroy(&ptr[i]);
-					}
-				}
-                
-				void	_shift_left(size_type position, size_type n)
-				{
-					if (empty())
-						return;
-					for (size_type i = position; i < m_size - n; i++)
-					{
-						_alloc.construct(&ptr[i], ptr[i + n]);
-						_alloc.destroy(&ptr[i + n]);
-					}
-				}
 	};
 
     template <class InputIterator1, class InputIterator2>
